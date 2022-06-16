a5087c5d94f46be83ca3d43c0d543bc5918a7e45
Freedom <freedom@localhost.localdomain>
2022-06-17 01:54:50 +0300

    forMonty

--- main.c
+++ main.c
@@ -1,74 +1,44 @@
 #include "monty.h"
 
 /**
- * main - entry into interpreter
- * @argc: argc counter
- * @argv: arguments
- * Return: 0 on success
+ * Globale data - collection of data instance
  */
-int main(int argc, char *argv[])
-{
-	int fd, ispush = 0;
-	unsigned int line = 1;
-	ssize_t n_read;
-	char *buffer, *token;
-	stack_t *h = NULL;
+data_t data;
 
-	if (argc != 2)
+/**
+ * main - main function
+ * @ac: arguments count
+ * @av: arguments vector
+ *
+ * Return: (Success) EXIT_SUCCESS
+ * ------- (Fail) EXIT_FAILURE
+ */
+int main(int ac, char **av)
+{
+	ssize_t n_read = 1;
+	size_t length = 0;
+	stack_t *stack = NULL;
+
+	memset((void *) &data, 0, sizeof(data));
+	if (ac != 2)
+		push_error(12);
+	data.filename = av[1];
+	data.fp = fopen(data.filename, "r");
+	if (data.fp == NULL)
+		push_error(14);
+	while ((n_read = getline(&data.line, &length, data.fp)) > 0)
 	{
-		printf("USAGE: monty file\n");
-		exit(EXIT_FAILURE);
-	}
-	fd = open(argv[1], O_RDONLY);
-	if (fd == -1)
-	{
-		printf("Error: Can't open file %s\n", argv[1]);
-		exit(EXIT_FAILURE);
-	}
-	buffer = malloc(sizeof(char) * 10000);
-	if (!buffer)
-		return (0);
-	n_read = read(fd, buffer, 10000);
-	if (n_read == -1)
-	{
-		free(buffer);
-		close(fd);
-		exit(EXIT_FAILURE);
-	}
-	token = strtok(buffer, "\n\t\a\r ;:");
-	while (token != NULL)
-	{
-		if (ispush == 1)
-		{
-			push(&h, line, token);
-			ispush = 0;
-			token = strtok(NULL, "\n\t\a\r ;:");
-			line++;
+		if (*data.line == '\n')
 			continue;
-		}
-		else if (strcmp(token, "push") == 0)
-		{
-			ispush = 1;
-			token = strtok(NULL, "\n\t\a\r ;:");
+		data.line_number++;
+		free(data.args);
+		if (split_line() < 0)
 			continue;
-		}
-		else
-		{
-			if (get_op_func(token) != 0)
-			{
-				get_op_func(token)(&h, line);
-			}
-			else
-			{
-				free_dlist(&h);
-				printf("L%d: unknown instruction %s\n", line, token);
-				exit(EXIT_FAILURE);
-			}
-		}
-		line++;
-		token = strtok(NULL, "\n\t\a\r ;:");
+		if (*data.args == NULL)
+			continue;
+		process_line(&stack);
 	}
-	free_dlist(&h); free(buffer);
-	close(fd);
-	return (0);
+	free_data();
+	free_dlistint(stack);
+	return (EXIT_SUCCESS);
 }
--- monty.h
+++ monty.h
@@ -1,15 +1,12 @@
-#ifndef MONTY_H
-#define MONTY_H
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <ctype.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
+#ifndef _MONTY_H
+#define _MONTY_H
 
-/* data structures */
+/* Libraries */
+#include <ctype.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
 
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
@@ -28,7 +25,7 @@ typedef struct stack_s
 } stack_t;
 
 /**
- * struct instruction_s - opcoode and its function
+ * struct instruction_s - opcode and its function
  * @opcode: the opcode
  * @f: function to handle the opcode
  *
@@ -41,29 +38,43 @@ typedef struct instruction_s
 	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
-/* opcodes related prototypes */
-void (*get_op_func(char *token1))(stack_t **stack, unsigned int line_number);
-void push(stack_t **h, unsigned int line_number, const char *n);
-void pop(stack_t **h, unsigned int line_number);
-void pint(stack_t **h, unsigned int line_number);
-void pall(stack_t **h, unsigned int line_number);
-void swap(stack_t **h, unsigned int line_number);
-void nop(stack_t **h, unsigned int line_number);
-void pchar(stack_t **h, unsigned int line_number);
-void pstr(stack_t **h, unsigned int line_number);
-void rotl(stack_t **h, unsigned int line_number);
-void rotr(stack_t **h, unsigned int line_number);
+/**
+ * struct instance_s - data instance
+ * @line: the readed line
+ * @args: the arguments
+ * @line_number: line number
+ * @fp: the file
+ * @filename: filename
+ * Description: data instance, line, args
+ */
+typedef struct instance_s
+{
+	char *line;
+	char **args;
+	char *filename;
+	int line_number;
+	FILE *fp;
+} data_t;
 
-/* calculator prototypes */
-void _add(stack_t **h, unsigned int line_number);
-void _sub(stack_t **h, unsigned int line_number);
-void _mul(stack_t **h, unsigned int line_number);
-void _div(stack_t **h, unsigned int line_number);
-void _mod(stack_t **h, unsigned int line_number);
+extern data_t data;
 
-/* doubly linked list related prototypes */
-int add_end_node(stack_t **h, int n);
-void delete_end_node(stack_t **h);
-void free_dlist(stack_t **h);
-
-#endif
+/* Prototypes */
+int process_line(stack_t **stack);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+void add_dnodeint(stack_t **stack, unsigned int line_number);
+void push_error(short int err_code);
+void free_data(void);
+void print_dlistint(stack_t **stack, unsigned int line_number);
+void free_dlistint(stack_t *stack);
+int split_line(void);
+void print_top(stack_t **stack, unsigned int line_number);
+void pop_top(stack_t **stack, unsigned int line_number);
+void swap_top(stack_t **stack, unsigned int line_number);
+int _isdigit(char *str);
+void nothing(stack_t **stack, unsigned int line_number);
+void add_top_two(stack_t **stack, unsigned int line_number);
+void sub_top_two(stack_t **stack, unsigned int line_number);
+void div_top_two(stack_t **stack, unsigned int line_number);
+void mul_top_two(stack_t **stack, unsigned int line_number);
+void mod_top_two(stack_t **stack, unsigned int line_number);
+#endif /* _MONTY_H */
